1e5722111895f648f12f5d56ed90b091f4439032
Nick <nick.benoit14@gmail.com>
2016-07-07 17:35:18 -0600

    added millis

--- src/errno_3.ino
+++ src/errno_3.ino
@@ -3,16 +3,16 @@
 #include <Wire.h>
 #include <Adafruit_MCP9808.h>
 
- 
+
 // define two tasks for Blink & AnalogRead
 void TaskBlink( void *pvParameters );
 void TaskAnalogRead( void *pvParameters );
 
 void TaskSensorRead(void *pvParameters);
- 
+
 // the setup function runs once when you press reset or power the board
 void setup() {
- 
+
   // Now set up two tasks to run independently.
   xTaskCreate(
     TaskBlink
@@ -21,7 +21,7 @@ void setup() {
     ,  NULL
     ,  2  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
     ,  NULL );
- 
+
 //  xTaskCreate(
 //    TaskAnalogRead
 //    ,  (const portCHAR *) "AnalogRead"
@@ -37,26 +37,34 @@ xTaskCreate(
     ,  NULL
     ,  1  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
     ,  NULL );
- 
+
   // Now the task scheduler, which takes over control of scheduling individual tasks, is automatically started.
 }
- 
+
 void loop()
 {
   // Empty. Things are done in Tasks.
 }
- 
+
+/**
+ * Sensor Read Functions
+ */
+
+void ReadTemp(){
+
+}
+
 /*--------------------------------------------------*/
 /*---------------------- Tasks ---------------------*/
 /*--------------------------------------------------*/
- 
+
 void TaskBlink(void *pvParameters)  // This is a task.
 {
   (void) pvParameters;
- 
+
   // initialize digital pin 13 as an output.
   pinMode(13, OUTPUT);
- 
+
   for (;;) // A Task shall never return or exit.
   {
     digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
@@ -65,14 +73,14 @@ void TaskBlink(void *pvParameters)  // This is a task.
     vTaskDelay( 1000 / portTICK_PERIOD_MS ); // wait for one second
   }
 }
- 
+
 void TaskAnalogRead(void *pvParameters)  // This is a task.
 {
   (void) pvParameters;
- 
+
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
- 
+
   for (;;)
   {
     // read the input on analog pin 0:
@@ -86,23 +94,23 @@ void TaskAnalogRead(void *pvParameters)  // This is a task.
 
 void TaskSensorRead(void *pvParameters){
 
-  // Task Setup 
+  // Task Setup
   Wire.begin(); //Begining everying on our I2C Bus
   Serial.begin(9600);
-  
+
   // Create sensor instances
-  Adafruit_MCP9808 sensor_temp = Adafruit_MCP9808(); 
+  Adafruit_MCP9808 sensor_temp = Adafruit_MCP9808();
 
   // Initialize Sensors
   if (!sensor_temp.begin()) {
     Serial.println("Couldn't find MCP9808!");
     while (1);
   }
-  
+
+  unsigned int lastRead;
   for(;;){
-   delay(1000);
-   Serial.println(sensor_temp.readTempC());
+    lastRead = millis();
+    Serial.println(sensor_temp.readTempC());
+    delay(1000);
   }
 }
-
-
