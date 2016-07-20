76d514a0578dfd444cd98445ef5e4565e9953ca5
Sailanarmo <rlramirez12@gmail.com>
2016-07-19 18:09:09 -0600

    SD Card is Writing

--- src/sensor.h
+++ src/sensor.h
@@ -1,6 +1,7 @@
 #ifndef SENSORS_H
 #define SENSORS_H
 
+
 void initialize_temp_ex(Adafruit_MCP9808* sensor){
   if (!sensor->begin(0x18)) {
     Serial.println("Couldn't find external MCP9808!");
@@ -16,8 +17,12 @@ void initialize_temp_in(Adafruit_MCP9808* sensor){
 }
 
 void read_temp(Adafruit_MCP9808* sensor){
-  Serial.print("\t");
+  File file = SD.open("sensors.txt", FILE_WRITE);
   Serial.print(sensor->readTempC());
+  Serial.print("\t");
+  file.println(sensor->readTempC());
+  file.print("\t");
+  file.close();
 }
 
 void initialize_baro(CoolSatBaro* sensor){
@@ -25,12 +30,17 @@ void initialize_baro(CoolSatBaro* sensor){
 }
 
 void read_baro(CoolSatBaro* sensor){
+  File file = SD.open("sensors.txt", FILE_WRITE);
   sensor->readBaro();
-  Serial.print("\t");
   Serial.print(sensor->getPressure());
+  Serial.print("\t");
+  file.print(sensor->getPressure());
+  file.print("\t");
+  file.close();
 }
 
 void read_light(){
+    File file = SD.open("sensors.txt", FILE_WRITE);
     float lightPin = 15; //anlaog light pin #
     float volt = 0.0; //voltage (volts)
     float RLDR = 0.0; //resistance (ohms)
@@ -43,16 +53,27 @@ void read_light(){
    RLDR = (1000.0 * (5 - volt )) / volt;
 	 lux = TOLUX * (pow(RLDR, TOLUXPWR));
 
-	 Serial.print("\t");
+   //Serial.print("\t");
+   //file.print("\t");
    Serial.print(lux);
+   Serial.print("\t");
+   file.print(lux);
+   file.print("\t");
+   file.close();
 }
 
 void read_uv(){
+    File file = SD.open("sensors.txt", FILE_WRITE);
     const int uvPin = 1; // UV sensor pin
     float uv = 0.0; // default
     uv = analogRead(uvPin); // reads value
     
-    Serial.print("\t");
+    //Serial.print("\t");
+    //file.print("\t");
     Serial.print(uv);
+    Serial.print("\t");
+    file.print(uv);
+    file.print("\t");
+    file.close();
 }
 #endif
