917fd1f65b18569a2723365fa006b03e5e6074b9
Jack Conrad Kiefer II <jack.c.kiefer@gmail.com>
2016-07-27 19:48:33 -0600

    Fixed decimal codes

--- src/messages.h
+++ src/messages.h
@@ -1,8 +1,8 @@
 #ifndef MESSAGES_H
 #define MESSAGES_H
 
-unsigned char TAKE_PHOTO = 98;
+unsigned char TAKE_PHOTO = 99; // press 'c' to take a photo with the [c]amera
 
-unsigned char DEPLOY_BOOM = 2;
+unsigned char DEPLOY_BOOM = 98; // press 'b' to deploy [b]oom
 
 #endif
