66b1ce5e73afc0666ed82e568d2474e4305f39e5
Nick <nick.benoit14@gmail.com>
2016-08-21 10:13:56 -0600

    float buffer instead of string buffer

--- src/sensor.hpp
+++ src/sensor.hpp
@@ -5,7 +5,7 @@
 class Sensor {
 public:
   Sensor(const char* sensor_name):name(sensor_name){}
-  virtual bool read(char* buff) = 0;
+  virtual bool read(float* buff) = 0;
   virtual bool init();
   const char* getName(){return name;}
 protected:
@@ -16,9 +16,11 @@ class TempSensor: public Sensor {
 public:
   TempSensor(const char* name, Adafruit_MCP9808* sensor, uint8_t address)
     :Sensor(name), sensor(sensor), address(address){}
-  bool read(char* buff){
-    if(!buff) return false;
-    return sprintf(buff, "%f");
+  bool read(float* buff){
+    // TODO null terminate buffer of floats
+    // if(!buff) return false;
+    // return sprintf(buff, "%f");
+    return false;
   }
   bool init(){return sensor->begin(0x18);}
 protected:
@@ -28,19 +30,19 @@ protected:
 
 class SensorReader{
 public:
-  SensorReader(Sensor** sensors, char** values):sensors(sensors), values(values){
+  SensorReader(Sensor** sensors, float* values):sensors(sensors), values(values){
     for(uint8_t i = 0; sensors[i] != (Sensor*) NULL; i++){
       // Do stuff to initialize files
-      values[i] = "*";
+      values[i] = -1;
     }
   }
 
-  void read(Sensor* sensor, Stream** outputs, char* buff);
-  char* getValue(const char* sensor_name){
-    int8_t sensor_index = findSensor(sensor_name);
-    if(sensor_index > 0){ return values[sensor_index]; }
-    return "*";
-  }
+  void read(Sensor* sensor, Stream** outputs, float* buff);
+  // float* getValue(const char* sensor_name){
+  //   int8_t sensor_index = findSensor(sensor_name);
+  //   if(sensor_index > 0){return values[sensor_index];}
+  //   return "*";
+  // }
 
 protected:
   int8_t findSensor(const char* sensor_name){
@@ -52,14 +54,14 @@ protected:
     return -1;
   }
   Sensor** sensors;
-  char** values;
+  float* values;
 };
 
-void SensorReader::read(Sensor* sensor, Stream** outputs, char* buff){
+void SensorReader::read(Sensor* sensor, Stream** outputs, float* buff){
   if(!sensor) return;
   sensor->read(buff);
   for(uint8_t i = 0; outputs[i]!= NULL; i++){
-    outputs[i]->print(buff);
+    // outputs[i]->print(buff);
   }
 }
 
