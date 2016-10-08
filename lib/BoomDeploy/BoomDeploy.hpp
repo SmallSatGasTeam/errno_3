da82a948d91a10a9db82611a7ae35533a01a530a
Nick <nick.benoit14@gmail.com>
2016-10-08 12:15:32 -0600

    add shouldDeployBoom test

---
+++ lib/BoomDeploy/BoomDeploy.hpp
@@ -0,0 +1,31 @@
+extern const float DEPLOY_MIN_PRESSURE;
+extern const float DEPLOY_MAX_PRESSURE;
+extern class Stream;
+
+bool shouldDeployBoom(bool deployed, bool initialized, bool confirmed, float pressure){
+  if(deployed == true) { return false; }
+  if(initialized == true && confirmed == true){ return true; }
+  if(pressure <= DEPLOY_MAX_PRESSURE && pressure >= DEPLOY_MIN_PRESSURE){ return true; }
+
+  return false;
+}
+
+char getMessage(Stream** input_streams){
+  char received_message = 0;
+    // We don't expect to receive commands from two streams at the same time. So this
+    // overwriting of the message shouldn't be a problem.
+
+  for (char i = 0; input_streams[i] != 0; ++i)
+  {
+    if (input_streams[i]->available())
+    {
+      received_message = input_streams[i]->read();
+      while (input_streams[i]->available())
+      {
+        input_streams[i]->read();
+      }
+      // Clear the rest of the buffer
+    }
+  }
+  return received_message;
+}
---
+++ test/deploy_boom/test_deploy_boom.cpp
@@ -0,0 +1,109 @@
+const float DEPLOY_MIN_PRESSURE = 5.0;
+const float DEPLOY_MAX_PRESSURE = 10.0;
+
+class Stream{
+public:
+  bool available(){ return true;}
+  char read(){ return 'a';}
+};
+
+#include <BoomDeploy.hpp>
+#include <unity.h>
+// #include <iostream>
+
+#ifdef UNIT_TEST
+
+
+// Method signature
+// bool shouldDeployBoom(bool deployed, bool initialized, bool confirmed, float pressure)
+void test_shouldDeployBoom(void){
+bool results[24] = {false};
+
+const float below_min = 1.0;
+const float in_range = 7.0;
+const float above_max = 15.0;
+
+// Truth table for each configuration this function can be called with. This gives
+// us 24 possible outcomes. for each boolean value there is two possibilites, true/false,
+// also for our pressure it could be in 1 of 3 different categories. below target range,
+// in target range, or above target range.
+// This gives us 2 * 2 * 2 * 3 or 24 different possibilites. This tests all of them.
+
+// When pressure is below minimum auto deploy
+
+  // All bools either true or false
+results[0] = shouldDeployBoom(true, true, true, below_min);
+TEST_ASSERT_FALSE(results[0]);
+results[1] = shouldDeployBoom(false, false, false, below_min);
+TEST_ASSERT_FALSE(results[1]);
+
+  // bools 1 false, 2 true
+results[2] = shouldDeployBoom(false, true, true, below_min);
+TEST_ASSERT_TRUE(results[2]);
+results[3] = shouldDeployBoom(true, false, true, below_min);
+TEST_ASSERT_FALSE(results[3]);
+results[4] = shouldDeployBoom(true, true, false, below_min);
+TEST_ASSERT_FALSE(results[4]);
+
+  // bools are 1 true, 2 false
+results[5] = shouldDeployBoom(true, false, false, below_min);
+TEST_ASSERT_FALSE(results[5]);
+results[6] = shouldDeployBoom(false, true, false, below_min);
+TEST_ASSERT_FALSE(results[6]);
+results[7] = shouldDeployBoom(false, false, true, below_min);
+TEST_ASSERT_FALSE(results[7])
+
+// ----- When pressure is in range
+
+  //follows same pattern as above
+results[8] = shouldDeployBoom(true, true, true, in_range);
+TEST_ASSERT_FALSE(results[8]);
+results[9] = shouldDeployBoom(false, false, false, in_range);
+TEST_ASSERT_TRUE(results[9]);
+
+results[10] = shouldDeployBoom(false, true, true, in_range);
+TEST_ASSERT_TRUE(results[10]);
+results[11] = shouldDeployBoom(true, false, true, in_range);
+TEST_ASSERT_FALSE(results[11]);
+results[12] = shouldDeployBoom(true, true, false, in_range);
+TEST_ASSERT_FALSE(results[12]);
+
+results[13] = shouldDeployBoom(true, false, false, in_range);
+TEST_ASSERT_FALSE(results[13]);
+results[14] = shouldDeployBoom(false, true, false, in_range);
+TEST_ASSERT_TRUE(results[14]);
+results[15] = shouldDeployBoom(false, false, true, in_range);
+TEST_ASSERT_TRUE(results[15]);
+
+// --- When pressure is above range
+
+  //follows same pattern as above
+results[16] = shouldDeployBoom(true, true, true, above_max);
+TEST_ASSERT_FALSE(results[16]);
+results[17] = shouldDeployBoom(false, false, false, above_max);
+TEST_ASSERT_FALSE(results[17]);
+
+results[18] = shouldDeployBoom(false, true, true, above_max);
+TEST_ASSERT_TRUE(results[18]);
+results[19] = shouldDeployBoom(true, false, true, above_max);
+TEST_ASSERT_FALSE(results[19]);
+results[20] = shouldDeployBoom(true, true, false, above_max);
+TEST_ASSERT_FALSE(results[20]);
+
+results[21] = shouldDeployBoom(true, false, false, above_max);
+TEST_ASSERT_FALSE(results[21]);
+results[22] = shouldDeployBoom(false, true, false, above_max);
+TEST_ASSERT_FALSE(results[22]);
+results[23] = shouldDeployBoom(false, false, true, above_max);
+TEST_ASSERT_FALSE(results[23]);
+}
+
+int main(int argc, char **argv) {
+    UNITY_BEGIN();
+    RUN_TEST(test_shouldDeployBoom);
+    UNITY_END();
+    return 0;
+}
+
+
+#endif
